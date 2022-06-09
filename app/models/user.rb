1bacb1e6ffe7ba22ddc684aeaa7ef18ec9b7e641
yRodrigo2219 <rodrigo2219dg@gmail.com>
2022-06-09 20:26:44 -0300

    Users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :todos
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, presence: true, uniqueness: true, { in: 3..20 }
-  validates :password,
-            length: { in: 6..20 },
-            if: -> { new_record? || !password.nil? }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, presence: true, uniqueness: true, length: { in: 3..20 }, case_sensitive: false
+  validates :name, presence: true, length: { in: 3..50 }
+  validates :password, length: { in: 6..20 }, if: -> { new_record? || !password.nil? }
 end
